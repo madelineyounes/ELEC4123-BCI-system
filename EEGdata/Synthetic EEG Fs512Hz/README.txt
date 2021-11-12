b7df56ad04b958d85a1db519e256a6e1a00bd65f
ExactEstimate <martinzhao99@yahoo.com>
2021-11-12 17:37:38 +1100

    Added titles to plots, and added magnitude response of real EEG signals

--- EEGdata/Synthetic EEG Fs512Hz/README.txt
+++
@@ -1,4 +0,0 @@
-Fs = 512Hz
-No 50Hz/60Hz noise present
-noisy data does not correspond to noiseless data.
-Noiseless data names are not given- you should figure out these bands yourself, then attempt it on noisy data.
--- Team12_Deliverable2.m
+++ Team12_Deliverable2.m
@@ -10,39 +10,76 @@
 
 clear;
 i = 1;
-load(sprintf('EEGdata/Real EEG data/rec%d.mat', i));
+load(sprintf('data/EEGdata/Real EEG data/rec%d.mat', i));
+L = length(channelData);
 figure(1);
-time = (0:length(channelData)-1)*(1/fs);
+sgtitle(sprintf('data/Real EEG Signal - rec%d.mat', i));
+
+% Plot EEG signal in time domain
+subplot(2, 1, 1);
+time = (0:L-1)*(1/fs);
 plot(time, channelData);
 if exist('channelNames', 'var')
     legend(channelNames);
 end
+title('Time Domain Plot');
 xlabel('Time (sec)');
 ylabel('Amplitude (uV)');
 grid on;
+
+% Plot EEG signal in frequency domain
+subplot(2, 1, 2);
+freq = linspace(-fs/2, fs/2, L);
+plot(freq, abs(fftshift(fft(channelData./1e6))));   % Convert from uV to V
+if exist('channelNames', 'var')
+    legend(channelNames);
+end
+title('Magnitude Response');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+grid on;
+
+% Annotate figure
+xlim([0 45]);
+xline(4, 'r', 'LineWidth', 1.5);    % Delta waves: 0–4 Hz
+xline(8, 'r', 'LineWidth', 1.5);    % Theta waves: 4–8 Hz
+xline(12, 'r', 'LineWidth', 1.5);   % Alpha waves: 8–12 Hz
+xline(40, 'r', 'LineWidth', 1.5);   % Beta waves: 12–40 Hz
+text(2, 0, 'Delta', 'color', 'r', 'FontSize', 16, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom');
+text(6, 0, 'Theta', 'color', 'r', 'FontSize', 16, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom');
+text(10, 0, 'Alpha', 'color', 'r', 'FontSize', 16, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom');
+text(26, 0, 'Beta', 'color', 'r', 'FontSize', 16, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom');
 
 %% View synthetic EEG data
 
 clear;
 fs = 512;
-noisy = true;
+
+noisy = false;
 % Load EEG signal
 if noisy
     % Noisy
-    fname = 'alpha1.mat';
-    eeg = load(sprintf('EEGdata/Synthetic EEG Fs512Hz/Noise/%s', fname)).noisy_EEGsig;
+    fname = 'beta1.mat';
+    eeg = load(sprintf('data/EEGdata/Synthetic EEG Fs512Hz/Noise/%s', fname)).noisy_EEGsig;
 else
     % Noiseless
-    i = 1;
-    eeg = load(sprintf('EEGdata/Synthetic EEG Fs512Hz/Noiseless/eeg%d.mat', i)).eeg;
+    i = 6;
+    eeg = load(sprintf('data/EEGdata/Synthetic EEG Fs512Hz/Noiseless/eeg%d.mat', i)).eeg;
 end
 L = length(eeg);
 
 % Plot EEG signal in time domain
 figure(2);
+if noisy
+    sgtitle(sprintf('Noisy - %s', fname));
+else
+    sgtitle(sprintf('Noiseless - eeg%d.mat', i));
+end 
+
 subplot(2, 1, 1);
 time = (0:L-1)*(1/fs);
 plot(time, eeg*1e6);    % Assuming original data is in units of V
+title('Time Domain Plot');
 xlabel('Time (sec)');
 ylabel('Amplitude (uV)');
 grid on;
@@ -51,6 +88,7 @@ grid on;
 subplot(2, 1, 2);
 freq = linspace(-fs/2, fs/2, L);
 plot(freq, abs(fftshift(fft(eeg))));
+title('Magnitude Response');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
 grid on;
